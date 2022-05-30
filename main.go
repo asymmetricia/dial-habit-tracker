4352c3444d700f2051a780875547e0bd90a31960
Tricia Bogen <tbogen@slack-corp.com>
2022-05-30 14:50:01 -0700

    clean up

--- main.go
+++ main.go
@@ -1,11 +1,5 @@
 package main
 
-import (
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
-)
-
 type Task struct {
 	Label string
 	Done  bool
@@ -24,32 +18,5 @@ var Sets = map[Color]Task{}
 
 const dataUrl = "dialhabittracker.dat"
 
-//func init() {
-//	data, err := storage.LoadResourceFromURI(storage.NewFileURI(dataUrl))
-//	if err != nil {
-//		log.Print(err)
-//		return
-//	}
-//	if err := json.Unmarshal(data.Content(), &Sets); err != nil {
-//		log.Print(err)
-//	}
-//}
-
 func main() {
-	app := app.New()
-	win := app.NewWindow("Dial Habit Tracker")
-
-	//var tabs []*container.TabItem
-	//for _, color := range []Color{Red, Orange, Yellow, Green} {
-	//	vbox := container.NewVBox(widget.NewLabel(string(color) + " Tasks"))
-	//	tabs = append(tabs, &container.TabItem{
-	//		Text:    string(color),
-	//		Icon:    nil,
-	//		Content: vbox,
-	//	})
-	//}
-	//tab := container.NewAppTabs(tabs...)
-	//win.SetContent(tab)
-	win.SetContent(container.NewVBox(widget.NewLabel("hi")))
-	win.ShowAndRun()
 }
